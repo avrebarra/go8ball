8eeb239b7de3c771eecdacb60aabe72107a6c007
Avreghly Barra <avreghly@gmail.com>
2019-10-23 00:02:52 +0700

    Add shaker module

---
+++ shaker.go
@@ -0,0 +1,42 @@
+package shaker
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var magicAnswers = [...]string{
+	// Positive outcomes
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes definitely",
+	"You may rely on it",
+	"As I see it, yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+
+	// Neutral outcomes
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+
+	// Negative outcomes
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful",
+}
+
+// Shake randomize Magic 8-Ball mechanism for picking
+// one answer from possible twenty answers
+func Shake() string {
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf(magicAnswers[rand.Intn(len(magicAnswers))])
+}
